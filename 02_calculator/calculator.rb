e141413e05008f95523aaa13e55323e4310549a5
Evan <ebay787@gmail.com>
2016-11-06 17:43:33 -0500

    Add existing file

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,22 @@
 #write your code here
+def add(x, y)
+	return x + y
+end
+def subtract(x, y)
+	return x - y
+end
+def sum arr
+	total = 0
+	arr.each do |i|
+		total += i
+	end
+	return total
+end
+
+def multiply arr
+	total = 0
+	arr.each do |i|
+		total *= i
+	end
+	return total
+end
